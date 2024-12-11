272550659ea721548ae211892d5e837d45e2f783
Tsigemariam <tsigemariamzewdu20@gmail.com>
2024-12-11 22:31:16 +0300

    installed react and taliwind and the first template

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Adjust this path based on your project structure
+    "./public/index.html"             // Include any other relevant files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
