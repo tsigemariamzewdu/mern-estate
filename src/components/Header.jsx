3c1b77b1e5fef2039f0a20f9846bb0c21df3e9b9
Tsigemariam <tsigemariamzewdu20@gmail.com>
2024-12-13 14:02:11 +0300

    fixed typo in header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,43 @@
 import React from 'react';
-import{FaSearch} from 'react-icons/fa' ;
-import  {Link} from react-router-dom;
-
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <Header className="bg-slate-200 shadow-md">
-        <div className='flex justify-betweeen items-center max-w-6xl mx-auto p-3'>
-            <Link to="/">
-        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>YourReal</span>
-            <span className='text-slate-700'>Estate</span>
-        </h1>
+    <div className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">YourReal</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
         </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-start">
-            <input type="text" 
-             placeholder='search..' 
-             className='bg-transparent focus:outline-none w-24 sm:w-64'/>
-            <FaSearch className='text-slate-600'/>
+          <input
+            type="text"
+            placeholder="search.."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+          />
+          <FaSearch className="text-slate-600" />
         </form>
-        <ul className='flex gap-4'>
-           <Link to="/"> <li className='hidden sm:inline text-slate-700 hover:underline '>Home</li></Link>
-           <Link to="/about"> <li className='hidden sm:inline text-slate-700 hover:underline '>About</li></Link>
-           <Link t0='/sign-in'> <li className=' sm:inline text-slate-700 hover:underline '>Sign in</li></Link>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <Link to="/sign-in">
+            <li className="sm:inline text-slate-700 hover:underline">Sign in</li>
+          </Link>
         </ul>
-        </div>
-    </Header>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
